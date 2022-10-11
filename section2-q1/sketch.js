5cc782181cd7369b977a2ce524ade3d5c3a96499
TomonagaNoboru <2110801h@stu.kobe-u.ac.jp>
2022-10-11 12:15:08 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
+  createCanvas(100,100)
+  noFill();
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+  if(i < 5){
+    stroke(0,0,255);
+    ellipse(50,50,i * 10 + 10);
+  }
+  else{
+    stroke(255,0,0);
+    ellipse(50,50,i * 10 + 10);
+    }
   }
 }
