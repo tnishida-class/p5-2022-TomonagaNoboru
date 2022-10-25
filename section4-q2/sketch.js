3af3563e6281ded2c9cd3bc4b672c9502558cec4
TomonagaNoboru <2110801h@stu.kobe-u.ac.jp>
2022-10-25 13:15:30 +0900

    Update sketch.js
    change section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -13,6 +13,39 @@ function setup(){
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
   for(let i = 0; i < scores.length; i++){ total += scores[i]; }
+  let strad = -PI / 2;
+  let edrad = 0;
+  let rx, ry , nx, ny;
+  for(let i = 0; i < scores.length; i++){
+    edrad = strad + 2 * PI * scores[i] / total
+    stroke(120);
+    strokeWeight(1);
+    arc(width / 2, height / 2 , width / 2 , height / 2, strad, edrad)
+    rx = width / 2 + width / 4 * Math.cos(edrad)
+    ry = height / 2 + height / 4 * Math.sin(edrad)
+    line(width / 2, height / 2, rx, ry)
+    //nx = width / 2 + width / 8 * Math.cos(edrad)
+    //ny = height / 2 + height / 8 * Math.sin(edrad)
+    //text(scores[i].toPrecision(3),nx ,ny);
+    strad = edrad;
+  }
+  for(let i = 0; i < scores.length; i++){
+    edrad = strad + 2 * PI * scores[i] / total
+    stroke(120);
+    strokeWeight(1);
+    rx = width / 2 + width / 4 * Math.cos(edrad)
+    ry = height / 2 + height / 4 * Math.sin(edrad)
+    line(width / 2, height / 2, rx, ry)
+    nx = width / 2 + width * 5 / 16 * Math.cos((strad + edrad) / 2 )
+    ny = height / 2 + height * 5 / 16 * Math.sin((strad + edrad) / 2)
+    per = (scores[i] / total) * 100
+    text(scores[i].toPrecision(3),nx - 10 ,ny + 10)
+    text(per.toPrecision(3),nx - 10 ,ny + 22);
+    text("      %",nx - 8 ,ny + 22)
+    strad = edrad;
+  }
 
+  console.log(strad, edrad)
   // BLANK[1]
+
 }
